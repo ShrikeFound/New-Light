f0505e120abdf8212321c41dec552c6abe504f39
Alfonso <61892420+ShrikeFound@users.noreply.github.com>
2021-03-26 19:04:47 -0700

    Update Artifacts.js

--- src/components/Artifacts.js
+++ src/components/Artifacts.js
@@ -6,7 +6,7 @@ const Artifacts = () => {
 
   const artifacts = [
     { Name: "Tezico's Multi-Material Container - Level 4", title: "A small orb made of stone covered in numerous bulbous sacks that pulse like a toad's throat. Several of the sacks have either burst or solidified. Four of them remain intact.", description: "With a command word, a sack can be opened and contain up to 6 cubic meters of a liquid or gaseous material. No other sack can contain the same material (or else). Each use has 1d20 chance of permanent failure. A sack can contain 6 hours of air." },
-    { Name: "Statis Sphere - Level 6", title: "a combination of hexagonal frames that lock in place when exposed to a current.", description:"Once locked, the heaxgons have to be carefully dismantled. The contents inside are in a temporal statis and cannot be affected by the outside world. The sphere itself is locked spatially relative to the largest mass." },
+    { Name: "Statis Sphere - Level 6", title: "a combination of hexagonal frames that lock in place when exposed to a current.", description:"Once locked, the heaxgons have to be carefully dismantled. The contents inside are in a temporal statis and cannot be affected by the outside world. The sphere itself is locked spatially relative to the largest mass. Each time the sphere is activated it has a 1d6 change of degradation." },
    
   ]
 
